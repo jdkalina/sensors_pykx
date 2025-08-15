c967648c272dbc789c86bf6f9d95f4c131d316cd
Josh Kalina <jdkalina@gmail.com>
2025-08-15 10:44:08 -0500

    Update fab_eg.py
    updated date filter in sensor lookup

--- fab_eg.py
+++ fab_eg.py
@@ -12,7 +12,7 @@ maxTime = kx_dt('2025.05.30T11:00:00.000')
 r = db.ees_run.select(columns=['tool_id','run_id','start_time','end_time'], where=[kx.Column('tool_id').isin(tool)])
 
 sd = db.ees_sensor_data.select(columns=['tool_id','time_stamps','data_float','data_long','data_str','sensor_id','ts_id'], 
-							   where=[kx.Column('min_time')> r['start_time'].min(), kx.Column('max_time')<= r['end_time'].max(), kx.Column('tool_id').isin(r['tool_id']), kx.Column('min_time').within(minTime, maxTime)])
+							   where=[kx.Column('min_time').within(minTime, maxTime), kx.Column('min_time')> r['start_time'].min(), kx.Column('max_time')<= r['end_time'].max(), kx.Column('tool_id').isin(r['tool_id']) ])
 
 tl = db.ees_tool_lookup.select(columns= ['tool_id','run_id','proc_type_id','proc_type_name'], where=[kx.Column('tool_id').isin(sd['tool_id']), kx.Column('run_id').isin(r['run_id'])])
 
