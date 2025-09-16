a54f9361caebeb5be0f97f10b064a08db5d8807a
Josh Kalina <jdkalina@gmail.com>
2025-09-16 09:34:26 -0500

    Update Query1pykx.py

--- Query1pykx.py
+++ Query1pykx.py
@@ -1,36 +1,28 @@
 import pykx as kx
 
+start = "2025.07.20"
+end = "2025.07.22"
+stdt = kx_dt(start)
+enddt = kx_dt(end)
+starttime = kx_time(start)
+endtime = kx_time(end)
+
 def main():
   tool_name = "IMV20J*"
   db = kx.DB(path='/data/kdb')
   def kx_dt(text):return kx.q(f'`date${text}')
   def kx_time(text):return kx.q(f'`datetime${text}')
-  
-  start = "2025.07.20"
-  end = "2025.07.22"
-  stdt = kx_dt(start)
-  enddt = kx_dt(end)
-  starttime = kx_time(start)
-  endtime = kx_time(end)
-  
   tinfo = db.ees_tool_lookup.select(columns=['tool_id','proc_type_id','tool_name'], where=[kx.Column('tool_name').like(tool_name)]) # returns back proc_type_id of 7139
-  
   sinfo = db.ees_sensor_lookup.select(columns = [kx.Column('id').name('sensor_id'),kx.Column('name'),kx.Column('proc_type_id')],where = [kx.Column('proc_type_id').isin(tinfo['proc_type_id']),kx.Column("name").like("*Pressure*"), kx.Column("name").like("IG*")]) # returns back sensor_id 2394585
-  
   kx.q['temptable'] = kx.q.lj(kx.q.xkey('proc_type_id',tinfo),kx.q.xkey('proc_type_id',sinfo))
   ts = kx.q('select name, tool_id, tool_name, proc_type_id, sensor_id, ts_id:sensor_id+4294967296*tool_id+sensor_id<0 from temptable')
-  
-  cinfo = db.ees_sensor_lookup.select(columns = [kx.Column('proc_type_id'),kx.Column("id"),kx.Column("name").name('recipe')],where=[kx.Column('proc_type_id').isin(tinfo['proc_type_id']),kx.Column('name').like('Recipe')]
-  
+  cinfo = db.ees_sensor_lookup.select(columns = [kx.Column('proc_type_id'),kx.Column("id"),kx.Column("name").name('recipe')],where=[kx.Column('proc_type_id').isin(tinfo['proc_type_id']),kx.Column('name').lower().like('*recipe*')]  
   runctx = db.ees_run_context.select(columns=['tool_id','run_id','start_time','ctx_value'],where=[kx.Column('date').within(stdt,enddt),kx.Column('tool_id').isin(tinfo['tool_id']),kx.Column('ctx_id').isin(cinfo['id']),kx.Column('start_time').within(starttime,endtime),kx.Column('ctx_value').like('Process.QA*')])  
   sd = db.ees_sensor_data.select(columns=['tool_id','sensor_id','ts_id','time_stamps','data_float'],where=[kx.Column('date').within(starttime, endtime),kx.Column('ts_id').isin(ts['ts_id']),kx.Column('min_time') >= runctx['start_time'].min()])  sd = kx.q.ungroup(sd)
-  # Key the field to join TSID into ctx_info for recipe info
   c = kx.q.xkey('proc_type_id', cinfo)
   ts = kx.q.xkey('proc_type_id', ts)
   df = kx.q.lj(c,ts)
   df = kx.q.unkey(0,df)
-  
-  # Key the field to join on SD to output
   df = kx.q.xkey('ts_id',df)
   sd = kx.q.xkey('ts_id', sd)
   df = kx.q.lj(sd,df)
