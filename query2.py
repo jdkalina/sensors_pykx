4b66519ae017cd746e29a13b868610cbe6c8ba6e
Josh Kalina <jdkalina@gmail.com>
2025-09-18 14:55:27 -0500

    Update query2.py

--- query2.py
+++ query2.py
@@ -30,7 +30,7 @@ def main():
     ep = leftMerge(s,ep,'signal_id','signal_id')
     dates = kx.q('date')
     counter = 0
-    
+    limit=100000
     for i in dates:
         r = db.ees_run_context.select(
             columns=['tool_id','tr_id','run_id','start_time','ctx_value'],
