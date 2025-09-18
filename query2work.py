de961aceba5c7597ac3de0519d42009f27ef3f14
Josh Kalina <jdkalina@gmail.com>
2025-09-18 15:39:13 -0500

    Update query2work.py

--- query2work.py
+++ query2work.py
@@ -12,7 +12,7 @@ for i in dates:
         er = db.ees_run.select(columns=[kx.Column('end_time').max(),kx.Column('start_time').min()],where=[ kx.Column('date').isin(i), kx.Column('tr_id').isin(r['tr_id'])])
         start_time = er['start_time'].min()
         end_time = er['end_time'].max()
-        sd = db.ees_sensor_data.select(columns=['tool_id','ts_id', 'data_float'], where=[kx.Column('date')==i, kx.Column('ts_id').isin(ep['id']), kx.Column('min_time') >= start_time, kx.Column('max_time')<= end_time])
+        sd = db.ees_sensor_data.select(columns=['tool_id','ts_id, data_float'], where=[kx.Column('date')==i, kx.Column('ts_id').isin(ep['ts_id']), kx.Column('min_time') >= start_time, kx.Column('max_time')<= end_time])
         sd = leftMerge(sd,r,'tool_id','tool_id')
         if counter == 0:
             df = sd.copy()
